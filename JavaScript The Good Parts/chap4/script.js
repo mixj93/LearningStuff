28321ea84ac92d018cec9c591358ef7b6c9b959a
mixj93 <mixj93@163.com>
2015-12-22 21:13:49 +0800

    finish chap 4

--- JavaScript The Good Parts/chap4/script.js
+++ JavaScript The Good Parts/chap4/script.js
@@ -10,7 +10,7 @@ Quo.prototype.get_status = function() {
 
 //创建一个Quo实例
 var myQuo = new Quo("confused");
-document.writeln(myQuo.get_status());
+document.writeln(myQuo.get_status()+"<br>");
 
 //汉诺塔
 var hanoi = function (disc, src, aux, dst) {
@@ -25,5 +25,23 @@ hanoi(4, 'SRC', 'AUX', 'DST');
 
 //walk_the_DOM
 var walk_the_DOM = function walk (node, func) {
-    
+    func(node);
+    node = node.firstChild;
+    while(node) {
+        walk(node, func);
+        node = node.nextSibling;
+    }
 }
+
+var getElementsByAttribute = function(att, value) {
+    var results = [];
+
+    walk_the_DOM(document.body, function(){
+        var actual = node.nodeType === 1 && node.getAttribute(att);
+        if(typeof actual === 'string' && (actual === value || typeof value !== 'string')) {
+            results.push(node);
+        }
+    });
+
+    return results;
+};
