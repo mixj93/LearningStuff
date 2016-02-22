2e32658d8cc07284ff540a5be2fc2b4c54443ae3
mixj93 <mixj93@163.com>
2016-02-22 11:41:41 +0800

    modified the readFile.py

--- python/python-read-file/readFile.py
+++ python/python-read-file/readFile.py
@@ -5,7 +5,7 @@ script, filename = argv
 txt = open(filename)
 
 print "Here's your file %r:" % filename
-print txt.read()
+var fileStr = txt.read()
 
 # print "Type the filename again:"
 # file_again = raw_input("> ")
