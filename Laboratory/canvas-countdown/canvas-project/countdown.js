f98e396ae1acc7ea41f7765a17d0ee1e78e90409
mixj93 <mixj93@163.com>
2016-03-02 17:21:08 +0800

    start canvas project

---
+++ Laboratory/canvas-countdown/canvas-project/countdown.js
@@ -0,0 +1,35 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+
+window.onload = function () {
+	var canvas = document.getElementById("canvas");
+	var content = canvas.getContext("2d");
+	canvas.width = WINDOW_WIDTH;
+	canvas.height = WINDOW_HEIGHT;
+
+	render(content);
+
+}
+
+function render (cxt) {
+	var hours = 12;
+	var minutes = 34;
+	var seconds = 56;
+
+	renderDigit(0, 0, parseInt(hours/10), cxt);
+}
+
+function renderDigit(x, y, num, cxt) {
+	cxt.fillStyle = "rgb(0, 102, 153);"
+
+	for (var i = 0; i < digit[num].length; i++) {
+		for (var j = 0; j < digit[num][i].length; j++) {
+			if(digit[num][i][j] == 1) {
+				cxt.beginPath();
+				cxt.arc();
+				cxt.closePath();
+				cxt.fill();
+			}
+		}
+	}
+}
